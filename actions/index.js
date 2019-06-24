5fb6427d790d8202c82fc91515613c5650cc6a0c
Pat  Pignataro <pat6284@msn.com>
2019-06-24 14:12:01 +0000

    Automatically backed up by Learn

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,6 @@
 import counterStore from '../stores/counterStore';
 
 module.exports = {
-  increment: () => counterStore.increment,
-  decrement: () => counterStore.decrement
+  increment: () => counterStore.increment(),
+  decrement: () => counterStore.decrement()
 };
